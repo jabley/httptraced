448948a27cc636ae8d0dabdfb8d4433a01e8152d
James Abley <james.abley@gmail.com>
2016-05-11 18:49:03 +0100

    Initial draft using net/http/httptrace
    This uses the new feature in Go 1.7.

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-Simple tool to monitor connection latency.
+[![Build Status](https://travis-ci.org/jabley/go-latency.svg?branch=master)](https://travis-ci.org/jabley/go-latency)
+
+Tool to monitor connection latency.
 
 We want a tool that will happily run for long periods, as well as single-shot.
 
-Hence this, rather than cron (which only gives us minute-by-minute granularity).
+Hence this, rather than curl and cron (which only gives us minute-by-minute granularity).
+
+It needs the new net/http/httptrace facility in Go 1.7.
+
+## Usage
+
+TODO update output using a version of Go that actually contains `net/http/httptrace`.
+
+```
+$ go-latency -i 4 https://github.com/
+{"timestamp":"2016-05-14T13:47:55.041794271Z","url":"https://github.com/","getConn":0,"gotConn":0,"ttfb":0,"dnsStart":0,"dnsDone":0,"connectStart":0,"connectDone":0,"wroteRequest":0,"total":1.2073199510000001}
+{"timestamp":"2016-05-14T13:47:59.046622346Z","url":"https://github.com/","getConn":0,"gotConn":0,"ttfb":0,"dnsStart":0,"dnsDone":0,"connectStart":0,"connectDone":0,"wroteRequest":0,"total":0.300005106}
+{"timestamp":"2016-05-14T13:48:03.043108335Z","url":"https://github.com/","getConn":0,"gotConn":0,"ttfb":0,"dnsStart":0,"dnsDone":0,"connectStart":0,"connectDone":0,"wroteRequest":0,"total":0.30343416}
+{"timestamp":"2016-05-14T13:48:07.045484402Z","url":"https://github.com/","getConn":0,"gotConn":0,"ttfb":0,"dnsStart":0,"dnsDone":0,"connectStart":0,"connectDone":0,"wroteRequest":0,"total":0.313531331}
+{"timestamp":"2016-05-14T13:48:11.044279722Z","url":"https://github.com/","getConn":0,"gotConn":0,"ttfb":0,"dnsStart":0,"dnsDone":0,"connectStart":0,"connectDone":0,"wroteRequest":0,"total":0.299610692}
+```
---
+++ main.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptrace"
+	"os"
+	"time"
+
+	_ "net/http/pprof"
+)
+
+var (
+	transport = &http.Transport{DisableKeepAlives: true}
+	client    = &http.Client{
+		Transport: transport,
+		Timeout:   time.Duration(10) * time.Second,
+	}
+
+	// CLI flags
+	help     bool
+	interval int64
+	count    int64
+)
+
+const (
+	usage = `
+go-latency [-help] [-i interval] [-c count] url
+
+go-latency will make a GET request to a URL and report on the timings.
+
+`
+
+	helpUsage   = "Display this help message"
+	helpDefault = false
+
+	intervalUsage   = "positive number of seconds to wait between making requests"
+	intervalDefault = 2
+
+	countUsage   = "positive number of requests to make. If set, then interval is assumed to be set"
+	countDefault = -1
+)
+
+func main() {
+	// parse flags
+	debug := flag.Bool("debug", false, "If true, you can debug this process at http://localhost:6060/debug/pprof/")
+
+	flag.BoolVar(&help, "help", helpDefault, helpUsage)
+	flag.BoolVar(&help, "h", helpDefault, helpUsage)
+
+	flag.Int64Var(&interval, "interval", intervalDefault, intervalUsage)
+	flag.Int64Var(&interval, "i", intervalDefault, intervalUsage)
+
+	flag.Int64Var(&count, "count", countDefault, countUsage)
+	flag.Int64Var(&count, "c", countDefault, countUsage)
+
+	flag.Parse()
+
+	if *debug {
+		go func() {
+			log.Println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
+
+	if help {
+		showUsage()
+		os.Exit(2)
+	}
+
+	// Enforce sensible defaults
+	if count < 0 {
+		count = countDefault
+	}
+	if interval < 0 {
+		interval = intervalDefault
+	}
+
+	URL := flag.Arg(0)
+
+	if URL == "" {
+		showUsage()
+		os.Exit(2)
+	}
+
+	poll(URL)
+}
+
+func showUsage() {
+	fmt.Fprintf(os.Stderr, usage)
+	flag.PrintDefaults()
+}
+
+type JSONTimestamp time.Time
+
+func (j *JSONTimestamp) MarshalJSON() ([]byte, error) {
+	stamp := fmt.Sprintf("\"%s\"", time.Time(*j).UTC().Format(time.RFC3339Nano))
+	return []byte(stamp), nil
+}
+
+type TimingContext struct {
+	StartTime            JSONTimestamp `json:"timestamp"`
+	URL                  string        `json:"url"`
+	GetConn              float64       `json:"getConn"`
+	GotConn              float64       `json:"gotConn"`
+	GotFirstResponseByte float64       `json:"ttfb"`
+	DNSStart             float64       `json:"dnsStart"`
+	DNSDone              float64       `json:"dnsDone"`
+	ConnectStart         float64       `json:"connectStart"`
+	ConnectDone          float64       `json:"connectDone"`
+	WroteRequest         float64       `json:"wroteRequest"`
+	Total                float64       `json:"total"`
+}
+
+func New(URL string) *TimingContext {
+	t := TimingContext{}
+	t.StartTime = JSONTimestamp(time.Now())
+	t.URL = URL
+	return &t
+}
+
+func (tc *TimingContext) Elapsed() float64 {
+	return time.Since(time.Time(tc.StartTime)).Seconds()
+}
+
+func poll(URL string) {
+	encoder := json.NewEncoder(os.Stdout)
+
+	tickInterval := time.Duration(interval) * time.Second
+
+	// channel used to do the initial poll
+	start := make(chan struct{})
+
+	// channel used to signal that we've done the required count of polls
+	done := make(chan struct{})
+
+	t := time.NewTicker(tickInterval)
+
+	if count != countDefault {
+		// stop the timer after count * interval seconds
+		go func() {
+			<-time.After(time.Duration(count-1) * tickInterval)
+			close(done)
+		}()
+	}
+
+	// This one weird trick to do the initial poll
+	go func() {
+		start <- struct{}{}
+	}()
+
+	for {
+		select {
+		case <-start:
+			doPoll(URL, encoder)
+		case <-done:
+			t.Stop()
+			return
+		case <-t.C:
+			doPoll(URL, encoder)
+		}
+	}
+}
+
+func doPoll(URL string, encoder *json.Encoder) {
+	tc, err := doIt(URL)
+
+	if err != nil {
+		loge(err)
+	}
+
+	if err := encoder.Encode(tc); err != nil {
+		loge(err)
+	}
+}
+
+func doIt(URL string) (*TimingContext, error) {
+	req, err := http.NewRequest("GET", URL, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	timingContext := New(URL)
+
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), &httptrace.ClientTrace{
+		GetConn:              func(hostPort string) { timingContext.GetConn = timingContext.Elapsed() },
+		GotConn:              func(ci httptrace.GotConnInfo) { timingContext.GotConn = timingContext.Elapsed() },
+		GotFirstResponseByte: func() { timingContext.GotFirstResponseByte = timingContext.Elapsed() },
+		DNSStart:             func(e httptrace.DNSStartInfo) { timingContext.DNSStart = timingContext.Elapsed() },
+		DNSDone:              func(e httptrace.DNSDoneInfo) { timingContext.DNSDone = timingContext.Elapsed() },
+		ConnectStart:         func(network, addr string) { timingContext.ConnectStart = timingContext.Elapsed() },
+		ConnectDone:          func(network, addr string, err error) { timingContext.ConnectDone = timingContext.Elapsed() },
+		WroteRequest:         func(e httptrace.WroteRequestInfo) { timingContext.WroteRequest = timingContext.Elapsed() },
+	}))
+
+	res, err := client.Do(req)
+
+	timingContext.Total = timingContext.Elapsed()
+
+	if err != nil {
+		return nil, err
+	}
+
+	res.Body.Close()
+
+	return timingContext, nil
+}
+
+func loge(err error) {
+	fmt.Fprintln(os.Stderr, err.Error())
+}
